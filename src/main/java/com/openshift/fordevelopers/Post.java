5fff793076ab2b8b85698ed292ade8d596cb9197
dubmart <78903371+dubmart@users.noreply.github.com>
2023-03-28 14:03:05 +0200

    Update Post.java

--- src/main/java/com/openshift/fordevelopers/Post.java
+++ src/main/java/com/openshift/fordevelopers/Post.java
@@ -24,7 +24,7 @@ public class Post extends PanacheMongoEntity {
     }
 
     public String getTitle() {
-        return new StringBuilder(title).reverse().toString();
+        return title;
     }
 
     public String getContent() {
